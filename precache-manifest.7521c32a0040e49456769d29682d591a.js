a35f49cb668abc848595695e5b56fed6a11daa2a
juangalbea <juangalbea@gmail.com>
2020-05-24 22:12:47 +0200

    Updates

---
+++ precache-manifest.7521c32a0040e49456769d29682d591a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4d08f7c31cac59b3133e6d0f6d9b5a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d50bbf1bc3908c507a5d",
+    "url": "/monsters-rolodex/static/css/main.008969f7.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monsters-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50bbf1bc3908c507a5d",
+    "url": "/monsters-rolodex/static/js/main.5f284d56.chunk.js"
+  },
+  {
+    "revision": "bc9b3e30e5d8354d0e21",
+    "url": "/monsters-rolodex/static/js/runtime-main.bdaefb98.js"
+  }
+]);
